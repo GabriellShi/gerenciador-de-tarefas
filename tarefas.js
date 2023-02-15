f3098831bba35beb1c974e21ccfedfe1bc04654e
GabriellShi <gabrieloliveirasantos196@gmail.com>
2023-02-15 11:42:18 +0000

    Finalizando o javascript e adicionaod algumas estilizações

--- tarefas.js
+++ tarefas.js
@@ -1,32 +1,58 @@
-// Fiz o selecionamento para puxar os codigos que irei aplicar o javascript atraves do id deles  
+// Fiz o selecionamento para puxar os codigos que irei aplicar o javascript atraves do id deles //  
 let inputNovaTarefa = document.querySelector('#inputNovaTarefa');
 let btnAddTarefa = document.querySelector('#btnAddTarefa');
 let listaTarefas = document.querySelector('#listaTarefas');
+let janelaEdicao = document.querySelector('#janelaEdicao');
+let janelaEdicaoFundo = document.querySelector('#janelaEdicaoFundo');
+let janelaEdicaoBtnFechar = document.querySelector('#janelaEdicaoBtnFechar');
+let btnAtualizarTarefa = document.querySelector('#btnAtualizarTarefa');
+let idTarefaEdicao = document.querySelector('#idTarefaEdicao');
+let inputTarefaNomeEdicao = document.querySelector('#inputTarefaNomeEdicao');
 
-// Adicionando o evento que sera excutado no input nova tarefa 
+// Adicionando o evento que sera excutado no input nova tarefa (evento para adicionar com 'Enter') // 
 inputNovaTarefa.addEventListener('keydown', (e) => {
-
     if(e.keyCode == 13) {
         let tarefa = {
             nome: inputNovaTarefa.value,
             id: gerarId(),
         }
         adicionarTarefa(tarefa);
-
     }
 });
 
-// Adicionando o evento de click que sera excutado ao clicar no botão adicionar tarefa  
+janelaEdicaoBtnFechar.addEventListener('click', (e) => {
+    alterarJanelaEdicao();
+});
+
+// Adicionando o evento de click que sera excutado ao clicar no botão adicionar tarefa //
 btnAddTarefa.addEventListener('click', (e) => {
     let tarefa = {
         nome: inputNovaTarefa.value,
         id: gerarId(),  
     }
     adicionarTarefa(tarefa);
-
-
 });
 
+btnAtualizarTarefa.addEventListener('click', (e) => {
+    e.preventDefault();
+    let idTarefa = idTarefaEdicao.innerHTML.replace('#', ''); 
+    let tarefa = {
+        nome: inputTarefaNomeEdicao.value,
+        id: idTarefa
+    }
+
+    let tarefaAtual = document.getElementById('' + idTarefa + '');
+    if(tarefaAtual) {
+        let li = criarTagLI(tarefa);
+        listaTarefas.replaceChild(li, tarefaAtual);
+        alterarJanelaEdicao();
+    } else {
+        alert('Elemento HTML não encontrado!')
+    }
+
+})
+
+// Criando a função para adicionar a tarefa na lista de baixo //
 function gerarId() {
     return Math.floor(Math.random() * 3000);
 }
@@ -63,16 +89,36 @@ function criarTagLI(tarefa) {
     li.appendChild(span);
     li.appendChild(div);
     return li;
-    
 }
 
 
 function editar(idTarefa) {
-    alert(idTarefa); 
+    let li = document.getElementById('' + idTarefa + '');
+    if(li) {
+        idTarefaEdicao.innerHTML = '#' + idTarefa; 
+        inputTarefaNomeEdicao.value = li.innerText;
+        alterarJanelaEdicao();
+    } else {
+        alert('Elemento HTML não encontrado!')
+    }
 }
 
+// Configurando o botão excluir, para poder excluir um item da lista //
 function excluir(idTarefa) {
-    alert(idTarefa); 
+    let confirmacao = window.confirm('Tem certeza que deseja excluir esse item?');
+    if(confirmacao) {
+        let li = document.getElementById(''+ idTarefa + '');
+        if (li) {
+            listaTarefas.removeChild(li);
+        } else {
+            alert('Elemento HTML não encontrado!')
+        }
+    } 
+}
+
+function alterarJanelaEdicao() {
+    janelaEdicao.classList.toggle('abrir');
+    janelaEdicaoFundo.classList.toggle('abrir');
 }
 
 
